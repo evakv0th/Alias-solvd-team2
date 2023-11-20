f851becc10e76814273e872a6e30f3768f40a741
kvothe eva <evakv0th@gmail.com>
2023-11-20 21:15:50 +0200

    #1 Setup Project

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Alias project!');
+});
+
+const startServer = async () => {
+  app.listen(port, () => {
+    console.log(`Server started at Port ${port}`);
+  });
+};
+
+startServer();
