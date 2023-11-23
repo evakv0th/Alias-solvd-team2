13dc1e71caac0ef29d19ef56e72fbbe197083a87
Ilya Lisov <ilya.lisov.yt@gmail.com>
2023-11-23 12:00:17 +0300

    #12 Add docker compose and databases initialization

---
+++ src/couchdb.init.ts
@@ -0,0 +1,39 @@
+import nano from "nano";
+
+export let couch: nano.ServerScope | undefined;
+
+export const chats_database = "chats";
+export const games_database = "games";
+export const rounds_database = "rounds";
+export const teams_database = "teams";
+export const users_database = "users";
+export const vocabularies_database = "vocabularies";
+
+export async function couchdbInit() {
+    const databases = [
+        chats_database,
+        games_database,
+        rounds_database,
+        teams_database,
+        users_database,
+        vocabularies_database
+    ];
+    const host = process.env.DATABASE_HOST!;
+    const port = process.env.DATABASE_PORT!;
+    const user = process.env.COUCHDB_USER!
+    const password = process.env.COUCHDB_PASSWORD!
+    let config = `http://${user}:${password}@${host}:${port}`
+    couch = nano(config);
+    databases.forEach(async (name) =>
+        await couch?.db.get(name, async (err) => {
+                if (err) {
+                    if (err.statusCode === 404) {
+                        await couch!.db.create(name);
+                        console.log(`Database ${name} was created.`)
+                    } else {
+                        console.error('Error checking database:', err);
+                    }
+                }
+            }
+        ));
+}
