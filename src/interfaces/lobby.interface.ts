bf39457885e53e763a718e9c898344d02156ce9e
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-24 19:13:21 +0100

    #10: gamelobby -> lobby.serivice update

--- src/interfaces/lobby.interface.ts
+++ src/interfaces/lobby.interface.ts
@@ -1,10 +1,14 @@
+import { ITeam } from "./team.interface";
+
 export interface ILobby {
     id: string;
     hostId: string;
     name: string;
-    members: string[];
+    teams: ITeam[];
     gameStarted: boolean;
     createdAt: string;
+    maxPlayers: number;
+    isPrivate: boolean;
   }
 
 export const tempLobbyArr: ILobby[] = [
@@ -12,8 +16,10 @@ export const tempLobbyArr: ILobby[] = [
     id: 'lobby1',
     hostId: '1',
     name: 'First Lobby',
-    members: ['1'],
+    teams: [],
     gameStarted: false,
-    createdAt: new Date().toISOString()
+    createdAt: new Date().toISOString(),
+    maxPlayers: 10,
+    isPrivate: false
   },
 ];
--- src/services/lobby.service.ts
+++ src/services/lobby.service.ts
@@ -1,24 +1,55 @@
 import { ILobby, tempLobbyArr } from '../interfaces/lobby.interface';
+import { ITeam } from '../interfaces/team.interface';
 
-export function createLobby(hostId: string, lobbyName: string): ILobby {
+export function createLobby(hostId: string, lobbyName: string, maxPlayers: number, isPrivate: boolean): ILobby {
   const newLobby: ILobby = {
-    // TODO: is the additional util need?
-    id: generateUniqueLobbyId(), 
+    id: generateUniqueLobbyId(),
     hostId,
     name: lobbyName,
-    members: [hostId],
+    teams: [], // Initialize with no teams
     gameStarted: false,
     createdAt: new Date().toISOString(),
+    maxPlayers,
+    isPrivate,
   };
   tempLobbyArr.push(newLobby);
   return newLobby;
 }
 
-export function joinLobby(userId: string, lobbyId: string): ILobby {
+export function joinLobby(userId: string, lobbyId: string, teamId: string): ILobby {
   const lobby = tempLobbyArr.find(lobby => lobby.id === lobbyId);
   if (!lobby) {
     throw new Error('Lobby not found');
   }
-  lobby.members.push(userId);
+  const team = lobby.teams.find(team => team.id === teamId);
+  if (!team) {
+    throw new Error('Team not found');
+  }
+  if (team.members.includes(userId)) {
+    throw new Error('User already in the team');
+  }
+  team.members.push(userId);
   return lobby;
+}
+
+
+export function selectTeam(userId: string, lobbyId: string, teamId: string): ITeam {
+  // Find the lobby by its ID
+  const lobby = tempLobbyArr.find(lobby => lobby.id === lobbyId);
+  if (!lobby) {
+    throw new Error('Lobby not found');
+  }
+
+  // Find the team within the lobby
+  const team = lobby.teams.find(team => team.id === teamId);
+  if (!team) {
+    throw new Error('Team not found');
+  }
+
+  if (team.members.includes(userId)) {
+    throw new Error('User already in the team');
+  }
+  
+  team.members.push(userId);
+  return team;
 }
