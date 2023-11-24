6d6598c61f24df8660295d59328202751e8ba8f8
Ilya Lisov <ilya.lisov.yt@gmail.com>
2023-11-24 12:36:49 +0300

    #13 Implement User service and repository

--- src/couchdb.init.ts
+++ src/couchdb.init.ts
@@ -1,6 +1,8 @@
 import nano from "nano";
+import {IUser} from "./interfaces/user.interface";
 
-export let couch: nano.ServerScope | undefined;
+export let couch: nano.ServerScope;
+export let usersDb: nano.DocumentScope<IUser>;
 
 export const chats_database = "chats";
 export const games_database = "games";
@@ -8,6 +10,15 @@ export const rounds_database = "rounds";
 export const teams_database = "teams";
 export const users_database = "users";
 export const vocabularies_database = "vocabularies";
+
+interface CouchDBViewMapFunction {
+  (doc: any): void;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function emit(field: string, doc: any): void {
+
+}
 
 export async function couchdbInit() {
   const databases = [
@@ -24,8 +35,8 @@ export async function couchdbInit() {
   const password = process.env.COUCHDB_PASSWORD!
   const config = `http://${user}:${password}@${host}:${port}`
   couch = nano(config);
-  databases.forEach(async (name) =>
-    await couch?.db.get(name, async (err) => {
+  for (const name of databases) {
+    await couch.db.get(name, async (err) => {
         if (err) {
           if (err.statusCode === 404) {
             await couch!.db.create(name);
@@ -35,5 +46,30 @@ export async function couchdbInit() {
           }
         }
       }
-    ));
+    );
+  }
+  usersDb = couch.use(users_database);
+
+  const userViews = {
+    _id: '_design/views',
+    views: {
+      byUsername: {
+        map: function (doc) {
+          if (doc.username) {
+            emit(doc.username, doc);
+          }
+        } as CouchDBViewMapFunction,
+      },
+    },
+  };
+  await initViews(usersDb, userViews);
+
+}
+
+async function initViews(database: nano.DocumentScope<any>, view: any) {
+  try {
+    await database.get("_design/views");
+  } catch (e: any) {
+    await database.insert(view)
+  }
 }
---
+++ src/services/user.service.ts
@@ -0,0 +1,32 @@
+import {IUser, IUserCreateSchema} from "../interfaces/user.interface";
+import {userRepository} from "../repositories/user.repository";
+
+class UserService {
+
+  async getById(id: string): Promise<IUser> {
+    return userRepository.getById(id);
+  }
+
+  async getByUsername(username: string): Promise<IUser> {
+    return userRepository.getByUsername(username);
+  }
+
+  async exists(username: string): Promise<boolean> {
+    return userRepository.exists(username);
+  }
+
+  async create(user: IUserCreateSchema): Promise<string> {
+    return userRepository.create(user);
+  }
+
+  async update(user: IUser): Promise<IUser> {
+    return userRepository.update(user);
+  }
+
+  async delete(id: string) {
+    await userRepository.delete(id);
+  }
+
+}
+
+export const userService = new UserService();
