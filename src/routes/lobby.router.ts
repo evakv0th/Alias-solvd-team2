a9bbd7537351857ed9e0925ec60ba397e3ad7c10
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-24 19:18:51 +0100

    #10: gamelobby -> lobby.routes update

--- src/routes/lobby.router.ts
+++ src/routes/lobby.router.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createLobby, joinLobby } from '../controllers/lobby.controller';
+import { createLobby, joinLobby, selectTeam } from '../controllers/lobby.controller';
 
 const lobbyRouter = express.Router();
 
 lobbyRouter.post('/create', createLobby);
 lobbyRouter.post('/join', joinLobby);
+lobbyRouter.post('/team/select', selectTeam );
 
 export default lobbyRouter;
