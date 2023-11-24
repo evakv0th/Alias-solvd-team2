9fa350052644853feb0d935a6beac9e26d89dc92
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-24 19:05:11 +0100

    #10: gamelobby -> team interface added

--- src/interfaces/lobby.interface.ts
+++ src/interfaces/lobby.interface.ts
@@ -7,4 +7,13 @@ export interface ILobby {
     createdAt: string;
   }
 
-export const tempLobbyArr: ILobby[] = [];
+export const tempLobbyArr: ILobby[] = [
+  {
+    id: 'lobby1',
+    hostId: '1',
+    name: 'First Lobby',
+    members: ['1'],
+    gameStarted: false,
+    createdAt: new Date().toISOString()
+  },
+];
---
+++ src/interfaces/team.interface.ts
@@ -0,0 +1,7 @@
+export interface ITeam {
+    id: string;
+    hostId: string;
+    name: string;
+    members: string[];
+}
+  
