e6a40c86b6fcdda365c7b52b9e8e6f11b08417e3
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-24 20:27:59 +0100

    #19: testing -> generateTocken tests update

--- src/application/tests/generateToken.test.ts
+++ src/application/tests/generateToken.test.ts
@@ -1,6 +1,42 @@
 import * as jwt from 'jsonwebtoken';
+import { IUser } from '../../interfaces/user.interface';
+import { generateAccessToken, generateRefreshToken } from '../utils/tokenForAuth/generateToken';
 
 
 describe('Token Generation', () => {
-
+    const user: IUser = {
+        id: '1',
+        username: 'test',
+        password: 'test',
+        createdAt: new Date(),
+        stats: {
+          roundPlayed: 1,
+          wordsGuessed: 1,
+        },
+      };
+    
+    beforeAll(() => {
+        process.env.SECRET_KEY = 'test-secret-key';
+        process.env.REFRESH_SECRET_KEY = 'test-refresh-secret-key';
+        process.env.ACCESS_TOKEN_EXP = '1h';
+        process.env.REFRESH_TOKEN_EXP = '7d';
+    });
+    
+    it('should generate an access token', () => {
+        generateAccessToken(user);
+        expect(jwt.sign).toHaveBeenCalledWith(
+          { userId: user.id, username: user.username },
+          process.env.SECRET_KEY,
+          { expiresIn: process.env.ACCESS_TOKEN_EXP },
+        );
+    });
+    
+    it('should generate a refresh token', () => {
+        generateRefreshToken(user);
+        expect(jwt.sign).toHaveBeenCalledWith(
+          { userId: user.id, username: user.username },
+          process.env.REFRESH_SECRET_KEY,
+          { expiresIn: process.env.REFRESH_TOKEN_EXP },
+        );
+    });
 });
