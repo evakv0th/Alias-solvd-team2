522933e14fec51321ff6f3fb9d9d001234764950
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-24 20:08:58 +0100

    #19: testing -> configuration

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
+
+export default config;
