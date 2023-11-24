f0d96a9def9882fbd9f75bfbbdf2fb995ca4149d
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-24 19:16:30 +0100

    #10: gamelobby -> lobby.controller update

--- src/controllers/lobby.controller.ts
+++ src/controllers/lobby.controller.ts
@@ -1,22 +1,35 @@
 import { Request, Response } from 'express';
 import * as lobbyService from '../services/lobby.service';
 
-export async function createLobby(req: Request, res: Response) {
+export async function createLobby(req: Request, res: Response): Promise<Response> {
   try {
-    const { hostId, name } = req.body;
-    const lobby = lobbyService.createLobby(hostId, name);
-    res.status(201).json(lobby);
+    const { hostId, name, maxPlayers, isPrivate } = req.body;
+    const lobby = lobbyService.createLobby(hostId, name, maxPlayers, isPrivate);
+    return res.status(201).json(lobby);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 }
 
-export async function joinLobby(req: Request, res: Response) {
+export async function joinLobby(req: Request, res: Response): Promise<Response> {
   try {
-    const { userId, lobbyId } = req.body;
-    const lobby = lobbyService.joinLobby(userId, lobbyId);
-    res.status(200).json(lobby);
+    const { userId, lobbyId, teamId } = req.body;
+    const lobby = lobbyService.joinLobby(userId, lobbyId, teamId);
+    return res.status(200).json(lobby);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export async function selectTeam(req: Request, res: Response): Promise<Response> {
+  try {
+    const { userId, lobbyId, teamId } = req.body;
+    const team = lobbyService.selectTeam(userId, lobbyId, teamId);
+    return res.status(200).json(team);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ error: error.message });
+    }
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 }
