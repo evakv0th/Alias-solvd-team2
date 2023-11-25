a49cebd89c7f434ac257b2bbecf9d93439034471
ruslan-andreev <ruslanandreev.tut@gmail.com>
2023-11-25 10:48:33 +0300

    # 21 Imlement validation check add return type

--- src/security/requestValidators.ts
+++ src/security/requestValidators.ts
@@ -1,35 +1,47 @@
 import { Request, Response, NextFunction } from "express";
 import HttpStatusCode from  '../application/utils/exceptions/statusCode';
 
-export function validateRegisterRequest() {
+export function validateRegisterRequest(): (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void {
   return(req: Request, res: Response, next: NextFunction) => {
     if (!req.body.username || !req.body.password) {
       return res
         .status(HttpStatusCode.BAD_REQUEST)
-        .json({ error: "Missing required fields" })
+        .json({ error: "Missing required fields" });
       }
       next();
     }
 }
 
-export function validateLoginRequest() {
+export function validateLoginRequest(): (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void {
   return(req: Request, res: Response, next: NextFunction) => {
     if(!req.body.username || !req.body.password) {
       return res
       .status(HttpStatusCode.BAD_REQUEST)
-      .json({ error: "Missing required fields" })
+      .json({ error: "Missing required fields" });
     }
     next();
   }
 }
 
-export function validateRefreshRequest() {
+export function validateRefreshRequest(): (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void {
   return(req: Request, res: Response, next: NextFunction) => {
     const { refreshToken } = req.body;
     if(!refreshToken) {
       return res
       .status(HttpStatusCode.BAD_REQUEST)
-      .json({ error: "Refresh token is missing" })
+      .json({ error: "Refresh token is missing" });
     }
     next();
   }
