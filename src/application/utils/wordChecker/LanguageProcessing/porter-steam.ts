7661756d49b9806f949752c3310f2c12ee008739
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-26 23:34:18 +0100

    #28: PorterStemmer algorithm updating the issue with nulables

--- src/application/utils/wordChecker/LanguageProcessing/porter-steam.ts
+++ src/application/utils/wordChecker/LanguageProcessing/porter-steam.ts
@@ -1,49 +1,50 @@
-/**
- * 
- * porterStemmer.ts
- * 
- */
-
 export class PorterStemmer {
 
-    // Private helper methods to define character classes used in regex patterns
-    private static getNonVowelCharsPattern(): string {
+    private static getNonVowelCharsPattern(): string 
+    {
         return '[^aeiou]';
     }
 
-    private static getVowelCharsPattern(): string {
+    private static getVowelCharsPattern(): string 
+    {
         return '[aeiouy]';
     }
 
-    private static getNonVowelSequencePattern(): string {
+    private static getNonVowelSequencePattern(): string 
+    {
         return `${PorterStemmer.getNonVowelCharsPattern()}[^aeiouy]*`;
     }
 
-    private static getVowelSequencePattern(): string {
+    private static getVowelSequencePattern(): string 
+    {
         return `${PorterStemmer.getVowelCharsPattern()}[aeiou]*`;
     }
 
-    private static getMeasureGreaterThanZeroPattern(): string {
+    private static getMeasureGreaterThanZeroPattern(): string 
+    {
         return `^(${PorterStemmer.getNonVowelSequencePattern()})?` +
             `${PorterStemmer.getVowelSequencePattern()}${PorterStemmer.getNonVowelSequencePattern()}`;
     }
 
-    private static getMeasureEqualsOnePattern(): string {
+    private static getMeasureEqualsOnePattern(): string 
+    {
         return `^(${PorterStemmer.getNonVowelSequencePattern()})?${PorterStemmer.getVowelSequencePattern()}` +
             `${PorterStemmer.getNonVowelSequencePattern()}(${PorterStemmer.getVowelSequencePattern()})?$`;
     }
 
-    private static getMeasureGreaterThanOnePattern(): string {
+    private static getMeasureGreaterThanOnePattern(): string 
+    {
         return `^(${PorterStemmer.getNonVowelSequencePattern()})?${PorterStemmer.getVowelSequencePattern()}` +
             `${PorterStemmer.getNonVowelSequencePattern()}${PorterStemmer.getVowelSequencePattern()}${PorterStemmer.getNonVowelSequencePattern()}`;
     }
 
-    private static getHasVowelPattern(): string {
+    private static getHasVowelPattern(): string 
+    {
         return `^(${PorterStemmer.getNonVowelSequencePattern()})?${PorterStemmer.getVowelCharsPattern()}`;
     }
 
-    private static getSuffixMappingsStep2(): Record<string, string> {
-        // Mappings for step 2
+    private static getSuffixMappingsStep2(): Record<string, string> 
+    {
         return {
             'ational' : 'ate',
             'tional' : 'tion',
@@ -69,7 +70,8 @@ export class PorterStemmer {
           };
     }
 
-    private static suffixMappingsStep3(): Record<string, string> {
+    private static suffixMappingsStep3(): Record<string, string> 
+    {
         return {
             'icate': 'ic',
             'ative': '',
@@ -81,142 +83,197 @@ export class PorterStemmer {
         };
     }
 
-    private static normalizeWord(word: string): string {
+    private static normalizeWord(word: string): string 
+    {
         const firstChar = word.charAt(0);
-        if (firstChar === 'y') {
+
+        if (firstChar === 'y') 
+        {
             word = firstChar.toUpperCase() + word.substr(1);
         }
+
         return word.toLowerCase().replace(/[^a-zA-Z]+/g, '');
     }
 
-    private static applyStep1a(word: string): string {
+    private static applyStep1a(word: string): string 
+    {
         let re = /^(.+?)(ss|i)es$/;
         let re2 = /^(.+?)([^s])s$/;
-        if (re.test(word)) { 
+
+        if (re.test(word)) 
+        { 
             word = word.replace(re, '$1$2'); 
-        } else if (re2.test(word)) {
+        } 
+        else if (re2.test(word)) 
+        {
             word = word.replace(re2, '$1$2'); 
         }
+
         return word;
     }
 
-    private static applyStep1b(word: string): string {
-        let re = /^(.+?)eed$/;
-        let re2 = /^(.+?)(ed|ing)$/;
-        if (re.test(word)) {
-          let fp = re.exec(word);
-          re = new RegExp(PorterStemmer.getMeasureGreaterThanZeroPattern());
-          if (re.test(fp[1])) {
-            re = /.$/;
-            word = word.replace(re, '');
-          }
-        } else if (re2.test(word)) {
-          let fp = re2.exec(word);
-          let stem = fp[1];
-          re2 = new RegExp(PorterStemmer.getHasVowelPattern());
-          if (re2.test(stem)) {
-            word = stem;
-            re2 = /(at|bl|iz)$/;
-            let re3 = new RegExp('([^aeiouylsz])\\1$');
-            let re4 = new RegExp(`^${PorterStemmer.getNonVowelSequencePattern()}` + 
-                `${PorterStemmer.getVowelSequencePattern()}[^aeiouwxy]$`);
-    
-            if (re2.test(word)) { 
-              word += 'e'; 
-            } else if (re3.test(word)) { 
-              re = /.$/; 
-              word = word.replace(re,''); 
-            } else if (re4.test(word)) { 
-                word += 'e'; 
+    private static applyStep1b(word: string): string 
+    {
+        const re = /^(.+?)eed$/;
+        const re2 = /^(.+?)(ed|ing)$/;
+        const match = re.exec(word);
+
+        if (match) 
+        {
+            const stem = match[1];
+            const measurePattern = new RegExp(PorterStemmer.getMeasureGreaterThanZeroPattern());
+            if (measurePattern.test(stem)) 
+            {
+                word = stem.replace(/.$/, '');
             }
-          }
+        } 
+        else 
+        {
+            const match2 = re2.exec(word);
+            if (match2) 
+            {
+                const stem = match2[1];
+                const hasVowelPattern = new RegExp(PorterStemmer.getHasVowelPattern());
+                if (hasVowelPattern.test(stem)) 
+                {
+                    word = stem;
+                    if (/(at|bl|iz)$/.test(word)) 
+                    { 
+                        word += 'e'; 
+                    } 
+                    else if (new RegExp('([^aeiouylsz])\\1$').test(word)) 
+                    { 
+                        word = word.replace(/.$/, ''); 
+                    } 
+                    else if (new RegExp(`^${PorterStemmer.getNonVowelSequencePattern()}${PorterStemmer.getVowelSequencePattern()}[^aeiouwxy]$`).test(word)) 
+                    { 
+                        word += 'e'; 
+                    }
+                }
+            }
+        }
+        
+        return word;
+    }
+
+
+    private static applyStep1c(word: string): string 
+    {
+        const re = new RegExp(`^(.*${PorterStemmer.getVowelCharsPattern()}.*)y$`);
+        const match = re.exec(word);
+
+        if (match) 
+        {
+            const stem = match[1];
+            word = stem + 'i';
+        }
+
+        return word;
+    }
+
+    private static applyStep2(word: string): string  
+    {
+        const re = /^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|biliti|logi)$/;
+        const match = re.exec(word);
+
+        if (match) 
+        {
+            const stem = match[1];
+            const suffix = match[2];
+            const reMeasure = new RegExp(PorterStemmer.getMeasureGreaterThanZeroPattern());
+            if (reMeasure.test(stem)) 
+            {
+                word = stem + PorterStemmer.getSuffixMappingsStep2()[suffix];
+            }
+        }
+
+        return word;
+    }
+
+    private static applyStep3(word: string): string 
+    {
+        const re = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/;
+        const match = re.exec(word);
+
+        if (match) 
+        {
+            const stem = match[1];
+            const suffix = match[2];
+            const reMeasure = new RegExp(PorterStemmer.getMeasureGreaterThanZeroPattern());
+            if (reMeasure.test(stem)) 
+            {
+                word = stem + PorterStemmer.suffixMappingsStep3()[suffix];
+            }
+        }
+
+        return word;
+    }
+
+    private static applyStep4(word: string): string 
+    {
+        const re = /^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|iti|ous|ive|ize)$/;
+        const re2 = /^(.+?)(s|t)(ion)$/;
+        let match = re.exec(word);
+
+        if (match) 
+        {
+            const stem = match[1];
+            const reMeasure = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
+            if (reMeasure.test(stem)) 
+            {
+                word = stem;
+            }
+        } 
+        else 
+        {
+            match = re2.exec(word);
+            if (match) 
+            {
+                const stem = match[1] + match[2];
+                const reMeasure = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
+                if (reMeasure.test(stem)) 
+                {
+                    word = stem;
+                }
+            }
+        }
+
+        return word;
+    }
+
+    private static applyStep5(word: string): string  
+    {
+        const re = /^(.+?)e$/;
+        const match = re.exec(word);
+
+        if (match) 
+        {
+            const stem = match[1];
+            const reMeasure = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
+            const reMeasureOne = new RegExp(PorterStemmer.getMeasureEqualsOnePattern());
+            const reCvc = new RegExp(`^${PorterStemmer.getNonVowelSequencePattern()}${PorterStemmer.getVowelCharsPattern()}[^aeiouwxy]$`);
+            if (reMeasure.test(stem) || (reMeasureOne.test(stem) && !reCvc.test(stem))) 
+            {
+                word = stem;
+            }
+        }
+
+        const reLl = /ll$/;
+        const reMeasure = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
+
+        if (reLl.test(word) && reMeasure.test(word)) 
+        {
+            word = word.replace(/.$/, '');
         }
         return word;
     }
 
-    private static applyStep1c(word: string): string {
-        let re = new RegExp(`^(.*${PorterStemmer.getVowelCharsPattern()}.*)y$`);
-        if (re.test(word)) {
-          let fp = re.exec(word);
-          let stem = fp[1];
-          word = stem + 'i';
+    public static stem(word: string): string 
+    {
+        if (word.length < 3) 
+        { 
+            return word; 
         }
-        return word;
-    }
-
-    private static applyStep2(word: string): string  {
-        let re = /^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|biliti|logi)$/;
-        if (re.test(word)) {
-          let fp = re.exec(word);
-          let stem = fp[1];
-          let suffix = fp[2];
-          re = new RegExp(PorterStemmer.getMeasureGreaterThanZeroPattern());
-          if (re.test(stem)) {
-            word = stem + PorterStemmer.getSuffixMappingsStep2()[suffix];
-          }
-        }
-        return word;
-    }
-
-    private static applyStep3(word: string): string {
-        let re = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/;
-        if (re.test(word)) {
-          let fp = re.exec(word);
-          let stem = fp[1];
-          let suffix = fp[2];
-          re = new RegExp(PorterStemmer.getMeasureGreaterThanZeroPattern());
-          if (re.test(stem)) {
-            word = stem + PorterStemmer.suffixMappingsStep3()[suffix];
-          }
-        }
-        return word;
-    }
-
-    private static applyStep4(word: string): string {
-        let re = /^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|iti|ous|ive|ize)$/;
-        let re2 = /^(.+?)(s|t)(ion)$/;
-        if (re.test(word)) {
-          let fp = re.exec(word);
-          let stem = fp[1];
-          re = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
-          if (re.test(stem)) {
-            word = stem;
-          }
-        } else if (re2.test(word)) {
-          let fp = re2.exec(word);
-          let stem = fp[1] + fp[2];
-          re2 = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
-          if (re2.test(stem)) {
-            word = stem;
-          }
-        }
-        return word;
-    }
-
-    private static applyStep5(word: string): string  {
-        let re = /^(.+?)e$/;
-        if (re.test(word)) {
-          let fp = re.exec(word);
-          let stem = fp[1];
-          re = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
-          let re2 = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
-          let re3 = new RegExp(`^${PorterStemmer.getNonVowelSequencePattern()}` + 
-            `${PorterStemmer.getVowelCharsPattern()}[^aeiouwxy]$`);
-          if (re.test(stem) || (re2.test(stem) && !(re3.test(stem)))) {
-            word = stem;
-          }
-        }
-        re = /ll$/;
-        let re2 = new RegExp(PorterStemmer.getMeasureGreaterThanOnePattern());
-        if (re.test(word) && re2.test(word)) {
-          re = /.$/;
-          word = word.replace(re, '');
-        }
-        return word;
-    }
-
-    public static stem(word: string): string {
-        if (word.length < 3) { return word; }
 
         word = PorterStemmer.normalizeWord(word);
         word = PorterStemmer.applyStep1a(word);
@@ -230,12 +287,16 @@ export class PorterStemmer {
         return word;
     }
 
-    public static async stemAsync(word: string): Promise<string> {
+    public static async stemAsync(word: string): Promise<string> 
+    {
         return new Promise((resolve, reject) => {
-            try {
+            try 
+            {
                 const stemmedWord = PorterStemmer.stem(word);
                 resolve(stemmedWord);
-            } catch (error) {
+            } 
+            catch (error) 
+            {
                 reject(error);
             }
         });
