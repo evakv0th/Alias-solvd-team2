7bb715ab12d08bfeb6545a6eb6b586cf0574d2d2
kvothe eva <evakv0th@gmail.com>
2023-11-27 21:50:18 +0200

    #44: services 404 when wrong id

--- src/services/round.service.ts
+++ src/services/round.service.ts
@@ -1,16 +1,24 @@
-import {roundRepository} from "../repositories/round.repository";
-import {IRound, IRoundCreateSchema} from "../interfaces/round.interface";
-import {gameService} from "./game.service";
+import { roundRepository } from '../repositories/round.repository';
+import { IRound, IRoundCreateSchema } from '../interfaces/round.interface';
+import { gameService } from './game.service';
+import HttpException from '../application/utils/exceptions/http-exceptions';
+import HttpStatusCode from '../application/utils/exceptions/statusCode';
 
 class RoundService {
-
   async getById(id: string): Promise<IRound> {
-    return roundRepository.getById(id);
+    try {
+      return roundRepository.getById(id);
+    } catch (error) {
+      throw new HttpException(
+        HttpStatusCode.NOT_FOUND,
+        'Round not found by id!',
+      );
+    }
   }
 
   async getAllByGameId(gameId: string): Promise<IRound[]> {
     const game = await gameService.getById(gameId);
-    const rounds = game.rounds.map(roundId => this.getById(roundId));
+    const rounds = game.rounds.map((roundId) => this.getById(roundId));
     return Promise.all(rounds);
   }
 
@@ -23,13 +31,26 @@ class RoundService {
   }
 
   async update(round: IRound): Promise<IRound> {
-    return roundRepository.update(round);
+    try {
+      return roundRepository.update(round);
+    } catch (error) {
+      throw new HttpException(
+        HttpStatusCode.NOT_FOUND,
+        'Round not found by id!',
+      );
+    }
   }
 
-  async delete(id: string) {
-    await roundRepository.delete(id);
+  async delete(id: string): Promise<void> {
+    try {
+      await roundRepository.delete(id);
+    } catch (error) {
+      throw new HttpException(
+        HttpStatusCode.NOT_FOUND,
+        'Round not found by id!',
+      );
+    }
   }
-
 }
 
 export const roundService = new RoundService();
