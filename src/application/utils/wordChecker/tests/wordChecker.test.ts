a8ab434d7072c03b25d3ca82053e5506234774d3
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-30 22:09:04 +0100

    #19: adding the tests for the WordChecker

---
+++ src/application/utils/wordChecker/tests/wordChecker.test.ts
@@ -0,0 +1,72 @@
+import isForbidden from '../wordChecker';
+
+describe('WordChecker', () => {
+  describe('isMessageValid', () => {
+    test('should return true when guessed word is a stem of word to check', () => {
+      const message = "today was an informal meeting";
+      const wordToCheck = "formal";
+      const isValid = isForbidden(message, wordToCheck);
+      expect(isValid).toBe(true);
+    });
+
+    test('should return true when no word is a stem of word to check', () => {
+      const message = "the sky is clear";
+      const wordToCheck = "cloud";
+      const isValid = isForbidden(message, wordToCheck);
+      expect(isValid).toBe(true);
+    });
+
+    test('should return false when a word is a stem of word to check', () => {
+      const message = "feeling of happiness";
+      const wordToCheck = "happy";
+      const isValid = isForbidden(message, wordToCheck);
+      expect(isValid).toBe(false);
+    });
+
+    test('should return false when message contains unrelated words', () => {
+      const message = "unrelated topic";
+      const wordToCheck = "related";
+      const isValid = isForbidden(message, wordToCheck);
+      expect(isValid).toBe(true);
+    });
+
+    test('should return true when message contains unrelated words', () => {
+        const message = "today was informal meeting";
+        const wordToCheck = "formal";
+        const isValid = isForbidden(message, wordToCheck);
+        expect(isValid).toBe(true);
+    });
+
+    test('should return true when message contains unrelated words', () => {
+        const message = "unhappiness followed by the rain";
+        const wordToCheck = "happy";
+        const isValid = isForbidden(message, wordToCheck);
+        expect(isValid).toBe(true);
+    });
+
+  });
+
+
+  describe('WordChecker - specific real cases', () => {
+    describe('isWordValid', () => {
+      it('should return false if the guessed word is the same as the word to check', () => {
+        expect(isForbidden('apple', 'apple')).toBe(false);
+      });
+  
+      it('should return true if the guessed word is different from the word to check', () => {
+        expect(isForbidden('banana', 'apple')).toBe(true);
+      });
+    });
+  
+    describe('isMessageValid', () => {
+      it('should return false if any word in the message is the same as the word to check', () => {
+        expect(isForbidden('I ate an apple this morning', 'apple')).toBe(false);
+      });
+  
+      it('should return true if no words in the message are the same as the word to check', () => {
+        expect(isForbidden('I had a banana for breakfast', 'apple')).toBe(true);
+      });
+    });
+  });
+
+});
