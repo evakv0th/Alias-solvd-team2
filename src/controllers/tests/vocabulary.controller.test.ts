96882d7fb964c7f7499a5930362c4762a60670f2
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-12-01 02:12:06 +0100

    #19: vocabulary.controller test update

--- src/controllers/tests/vocabulary.controller.test.ts
+++ src/controllers/tests/vocabulary.controller.test.ts
@@ -47,5 +47,86 @@ describe('VocabularyController.getById', () => {
       expect(res.status).toHaveBeenCalledWith(HttpStatusCode.NOT_FOUND);
       expect(res.json).toHaveBeenCalledWith({ error: 'Not found' });
     });
+});
+  
+
+describe('VocabularyController.create', () => {
+    it('should create a new vocabulary and return its id', async () => {
+      const req = mockRequest({}, { words: ['word1', 'word2'] });
+      const res = mockResponse();
+  
+      (vocabularyService.create as jest.Mock).mockResolvedValue('newVocabId');
+  
+      await vocabularyController.create(req as Request, res as Response);
+  
+      expect(res.status).toHaveBeenCalledWith(HttpStatusCode.CREATED);
+      expect(res.json).toHaveBeenCalledWith({ id: 'newVocabId' });
+    });
+  
+    it('should handle errors during vocabulary creation', async () => {
+      const req = mockRequest({}, { words: ['word1', 'word2'] });
+      const res = mockResponse();
+  
+      (vocabularyService.create as jest.Mock).mockRejectedValue(new HttpException(HttpStatusCode.INTERNAL_SERVER_ERROR, 'Error creating vocabulary'));
+  
+      await vocabularyController.create(req as Request, res as Response);
+  
+      expect(res.status).toHaveBeenCalledWith(HttpStatusCode.INTERNAL_SERVER_ERROR);
+      expect(res.json).toHaveBeenCalledWith({ error: 'Error creating vocabulary' });
+    });
   });
+  
+  describe('VocabularyController.update', () => {
+    it('should update an existing vocabulary and return the updated vocabulary', async () => {
+      const req = mockRequest({ id: 'vocabId' }, { words: ['word1', 'word2'] });
+      const res = mockResponse();
+      const updatedVocabulary = { _id: 'vocabId', words: ['word1', 'word2'] };
+  
+      (vocabularyService.update as jest.Mock).mockResolvedValue(updatedVocabulary);
+  
+      await vocabularyController.update(req as Request, res as Response);
+  
+      expect(res.status).toHaveBeenCalledWith(HttpStatusCode.OK);
+      expect(res.json).toHaveBeenCalledWith(updatedVocabulary);
+    });
+  
+    it('should handle errors during vocabulary update', async () => {
+      const req = mockRequest({ id: 'vocabId' }, { words: ['word1', 'word2'] });
+      const res = mockResponse();
+  
+      (vocabularyService.update as jest.Mock).mockRejectedValue(new HttpException(HttpStatusCode.NOT_FOUND, 'Vocabulary not found'));
+  
+      await vocabularyController.update(req as Request, res as Response);
+  
+      expect(res.status).toHaveBeenCalledWith(HttpStatusCode.NOT_FOUND);
+      expect(res.json).toHaveBeenCalledWith({ error: 'Vocabulary not found' });
+    });
+  });
+  
+  describe('VocabularyController.delete', () => {
+    it('should delete a vocabulary and return no content', async () => {
+      const req = mockRequest({ id: 'vocabId' });
+      const res = mockResponse();
+  
+      (vocabularyService.delete as jest.Mock).mockResolvedValue({});
+  
+      await vocabularyController.delete(req as Request, res as Response);
+  
+      expect(res.status).toHaveBeenCalledWith(HttpStatusCode.NO_CONTENT);
+      expect(res.send).toHaveBeenCalled();
+    });
+  
+    it('should handle errors during vocabulary deletion', async () => {
+      const req = mockRequest({ id: 'vocabId' });
+      const res = mockResponse();
+  
+      (vocabularyService.delete as jest.Mock).mockRejectedValue(new HttpException(HttpStatusCode.NOT_FOUND, 'Vocabulary not found'));
+  
+      await vocabularyController.delete(req as Request, res as Response);
+  
+      expect(res.status).toHaveBeenCalledWith(HttpStatusCode.NOT_FOUND);
+      expect(res.json).toHaveBeenCalledWith({ error: 'Vocabulary not found' });
+    });
+});
+
   
