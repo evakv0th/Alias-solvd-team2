656b572c530abce4da1ab5b9227a8837d483fa20
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-12-04 20:19:40 +0100

    #19: jest.config update

--- jest.config.ts
+++ jest.config.ts
@@ -18,10 +18,10 @@ const config: Config.InitialOptions = {
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
+      branches: 30,
+      functions: 50,
+      lines: 60,
+      statements: 60
     }
   },
 };
